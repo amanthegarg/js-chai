a09bc37d8d1b9e0f0eb8fa67c6bd07062139fc8a
amanthegarg <gargaman2003@gmail.com>
2024-12-26 19:00:41 +0530

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all JS code as newer version of JS
+
+// alert(3+3)//we are using nodejs not browser
+
+    console.log(
+    3
+    +
+    3);
+    //code readability should be high
+
+    console.log("Aman")
+
+let name = "Aman"
+let age = 18
+let isLogIn = false
+let state = null
+let a;
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+// boolean => true/false
+// undefined
+// null => standalone value
+// Symbol => unique
+
+//object
+
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
+
