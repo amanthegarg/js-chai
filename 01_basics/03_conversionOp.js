c99cda56c4bea842cabced13a901188f98ed9fb1
amanthegarg <gargaman2003@gmail.com>
2025-01-06 23:26:13 +0530

    completed till comparisions on 6/1/25

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,73 @@
+let score = "Aman"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//NaN is a type of number in js.
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Aman"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Aman" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************************************Operations***********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let str1 = "hello"
+let str2 = " aman"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2")//12
+// console.log("1" + 2)//12
+// console.log("1" + 2 + 2)//122
+// console.log(2 + "1" + 2)//212
+// console.log(2 + 1 + "1" )//31
+
+// console.log((3 + 2) * 5 % 3); // use paranthesis
+
+//console.log(+true) // 1
+// console.log(+"") //0
+
+//console.log(true+)//not valid
+
+let num1, num2, num3
+num1 = num2 =num3 = 2+2 //no need to show that you are intelligent by these methods
+
+let gameCounter = 100;
+gameCounter++;//post increment 
+++gameCounter;//pre increment 
+console.log(gameCounter);
+
+
+
+
+
+
