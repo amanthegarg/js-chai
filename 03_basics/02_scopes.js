2337d0b992ee22b73a307970d2b6d6bba4e65ca8
amanthegarg <gargaman2003@gmail.com>
2025-01-10 15:12:34 +0530

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,16 +1,54 @@
-var c = 300
-let a = 300
-if (true) {
-  let a = 10;
-  const b = 30;
-  var c = 30;
-  console.log("inner : ",a);
+// var c = 300
+// let a = 300
+// if (true) {
+//   let a = 10;
+//   const b = 30;
+//   var c = 30;
+//   console.log("inner : ",a);
   
-}
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// }
+
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+function one()
+{
+    const username = "Aman"
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);//out of scope
+
+    two()
     
 }
-console.log(a);
-// console.log(b);
-console.log(c);
+// one()
+
+if (true) {
+    const username = "Aman"
+    if (username==="Aman") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);//out of scope
+    
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++ Intersting +++++++++++++++++++++++++++++++++++++++++++++
+
+addone(5) // here we can do it
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+// addtwo(5) // in this declaration of function we Cannot access 'addtwo' before initialization
+const addtwo = function (num){
+    return num + 2
+}
+
+addtwo(5)
